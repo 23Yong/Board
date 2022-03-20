266f6aedf30e883619e9f6b6ad3f956428d695e2
23Yong <66981851+23Yong@users.noreply.github.com>
2022-03-20 17:27:25 +0900

    게시글 조회 및 수정기능 추가

---
+++ src/main/java/spring/board/controller/form/PostEditForm.java
@@ -0,0 +1,16 @@
+package spring.board.controller.form;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter @Setter
+public class PostEditForm {
+
+    private Long postId;
+    @NotEmpty(message = "게시글의 제목은 반드시 있어야 합니다.")
+    private String title;
+    @NotEmpty(message = "게시글의 내용은 반드시 있어야 합니다.")
+    private String content;
+}
