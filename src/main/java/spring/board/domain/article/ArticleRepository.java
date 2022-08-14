a2379feae97491c522c9443645fac1a937e1b7c3
23Yong <1239824@naver.com>
2022-08-14 23:27:14 +0900

    #10 - repository interface
    보기 좋게끔 수정

--- src/main/java/spring/board/domain/article/ArticleRepository.java
+++ src/main/java/spring/board/domain/article/ArticleRepository.java
@@ -1,8 +1,6 @@
 package spring.board.domain.article;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
