3b634f290297017938c0c15c38d54f2ac29e76b9
23Yong <1239824@naver.com>
2022-08-24 14:57:36 +0900

    #28 - 댓글 기능 구현
    댓글 기능의 테스트로 명세해 놨던 걸 구현 후 
    전체 테스트 통과 확인

--- src/main/java/spring/board/service/ArticleCommentService.java
+++ src/main/java/spring/board/service/ArticleCommentService.java
@@ -1,14 +1,19 @@
 package spring.board.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import spring.board.domain.article.ArticleRepository;
+import spring.board.domain.articlecomment.ArticleComment;
 import spring.board.dto.ArticleCommentDto;
 import spring.board.domain.articlecomment.ArticleCommentRepository;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Slf4j
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 @Service
@@ -18,21 +23,34 @@ public class ArticleCommentService {
     private final ArticleRepository articleRepository;
 
     public List<ArticleCommentDto> searchArticleComments(Long articleId) {
-        return List.of();
+        return articleCommentRepository.findByArticle_Id(articleId)
+                .stream().map(ArticleCommentDto::from)
+                .collect(Collectors.toList());
     }
 
     @Transactional
     public void saveArticleComment(ArticleCommentDto dto) {
-
+        try {
+            articleCommentRepository.save(dto.toEntity(articleRepository.getReferenceById(dto.articleId())));
+        } catch (EntityNotFoundException e) {
+            log.warn("댓글 저장 실패. 댓글의 게시글을 찾을 수 없습니다 - dto: {}", dto);
+        }
     }
 
     @Transactional
     public void updateArticleComment(ArticleCommentDto dto) {
-
+        try {
+            ArticleComment articleComment = articleCommentRepository.getReferenceById(dto.id());
+            if (dto.content() != null) {
+                articleComment.changeContent(dto.content());
+            }
+        } catch(EntityNotFoundException e) {
+            log.warn("댓글 업데이트 실. 댓글을 찾을 수 없습니다 - dto:{}", dto);
+        }
     }
 
     @Transactional
-    public void deleteArticleComment(long articleCommentId) {
-
+    public void deleteArticleComment(Long articleCommentId) {
+        articleCommentRepository.deleteById(articleCommentId);
     }
 }
