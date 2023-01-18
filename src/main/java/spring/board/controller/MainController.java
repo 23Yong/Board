136982db1c16fae9a5e38ed1b3c6174a831785e2
23Yong <1239824@naver.com>
2023-01-18 16:29:05 +0900

    #88 - Swagger UI 적용
    간단한 의존성 추가로 즉시 Swagger 문서화가 진행된다.
    이 때 Spring Data Rest가 자동으로 만들어준 api를 
    Swagger에 노출 시키기 위해 추가 의존성을 사용한다.

--- src/main/java/spring/board/controller/MainController.java
+++ src/main/java/spring/board/controller/MainController.java
@@ -3,9 +3,18 @@ package spring.board.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * <p>
+ *     메인 컨트롤러
+ * </p>
+ */
 @Controller
 public class MainController {
 
+    /**
+     * 게시판 홈 /articles로 포워딩 시킨다
+     * @return
+     */
     @GetMapping("/")
     public String root() {
         return "forward:/articles";
